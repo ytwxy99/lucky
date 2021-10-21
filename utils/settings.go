67d7154787c7086b4f5c67e47924764511c366d9
wangxiaoyu <wangxiaoyu.tracy@bytedance.com>
2021-10-21 20:43:37 +0800

    add sina source

--- utils/settings.go
+++ utils/settings.go
@@ -1,6 +1,7 @@
 package utils
 
 var Sohu = "http://q.stock.sohu.com/hisHq?"
+var Sina = "http://hq.sinajs.cn/list=sz150304"
 var TradeStartTime = "20210101"
 var TradeEndTime = "20211015"
 var PageSize int = 20
